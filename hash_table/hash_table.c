2c590e3f82a3ace8f4fede00fca16c95622a3f74
Rick Zucker <rzucker4@comcast.net>
2019-09-02 21:27:08 -0700

    Added hash computation as separate function, and address as information in addition to key

--- hash_table/hash_table.c
+++ hash_table/hash_table.c
@@ -29,33 +29,39 @@ void print_hash_entry( e )
     t_elem *traverse = &e;
 
     while (traverse != NULL) {
-        printf( "Key value is %d, ", traverse->key );
+        printf( "Key value is %d, address %d   ", traverse->key, traverse->street_num );
 	traverse = traverse->next;
     }
     printf( "\n" );
 }
 
-void insert_hash( hash_elem, val, address )
+void insert_hash( hash_elem, key, address )
    t_elem* hash_elem;
-   int val;
+   int key;
    int address;
 {
    t_elem *tmp;
 
-   if (hash_elem->key == NOT_OCCUPIED) { /* Nothing is stored at this location. Just use location from array */
-       hash_elem->key        = val;
+   if (hash_elem->key == NOT_OCCUPIED) { /* Nothing is stored at this location. Just use location in array */
+       hash_elem->key        = key;
        hash_elem->next       = NULL;
        hash_elem->street_num = address;
        return;
    } else { /* There is a collision. Need to create a new element */
        tmp = malloc(sizeof(t_elem));
-       tmp->key  = val;              /* Put new value in new element */
+       tmp->key  = key;              /* Put new value in new element */
        tmp->street_num = address;
        tmp->next = hash_elem-> next; /* Point to wherever first location points */
        hash_elem->next = tmp;        /* Putting new element at front of list */
-       printf("Added %d after first element\n", val );
+       printf("Added %d after first element\n", key );
    }
+}
 
+
+int hash_func( key )
+    int key;
+{
+    return (key % HASH_SIZE);
 }
 
 int main()
@@ -68,8 +74,8 @@ int main()
 	hash_table [i].next = NULL;
     }
     
-    for (i = 0; i < (sizeof(values) / sizeof(values [0])); i++) {
-       insert_hash( &(hash_table[ (values [i] % HASH_SIZE) ]), values [i] );
-       print_hash_entry( hash_table[ (values [i] % HASH_SIZE) ] );
+    for (i = 0; i < (sizeof( values ) / sizeof( values [0] )); i++) {
+       insert_hash( &(hash_table[ hash_func( values [i] )]), values [i], i*13 );
+       print_hash_entry( hash_table[ hash_func( values [i] ) ] );
     }
 }
